9ce1dede5218f8c303e51352efc9821acb07aebf
Christopher Pineda <Christopher.Pineda59@myhunter.cuny.edu>
2022-04-08 00:21:22 -0400

    Shifted 75% of the code in both Debit.js and Credit.js to fix credit,debit,and account balances

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // src/App.js
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
@@ -12,15 +11,134 @@ class App extends Component {
   constructor(props) {  // Create and initialize state
     super(props);
     this.state = {
-      accountBalance: 14568.27,
+      accountBalance: 0,
       debitBalance: 0,
       creditBalance: 0,
+      // DEBIT PORTION //
+      debit_statement: '',
+      debit_value: '',
+      debit_array: [],
+      // CREDIT PORTION //
+      credit_statement: '',
+      credit_array: [],
+      credit_value: '',
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '07/23/96',
       }
     }
   }
+
+  componentDidMount() {
+    /////////////////////////////////THIS IS FOR DEBIT VALUES /////////////////////////////////////////////
+    let debit_data = null;
+    // THIS PORTION PARSES THROUGH DEBITS API AND APPENDS IT TO OUR EMPTY STATEMENT LIST ARRAY
+    fetch('https://moj-api.herokuapp.com/debits')
+      .then(result => result.json())
+      .then(data => {
+        debit_data = data;
+        for (let i = 0; i < 10; i++) {
+          var joined = this.state.debit_array.concat('$' + debit_data[i].amount + ' ' + debit_data[i].description + ' ' + debit_data[i].date.slice(0, 10));
+          this.setState({
+            debit_array: joined,
+            debitBalance: this.state.debitBalance + debit_data[i].amount,
+            accountBalance: this.state.accountBalance - debit_data[i].amount
+          })
+        }
+      });
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////THIS IS FOR CREDIT VALUES////////////////////////////////////////////////////////////////////////////
+    let credit_data = null;
+    fetch('https://moj-api.herokuapp.com/credits')
+      .then(result => result.json())
+      .then(data => {
+        credit_data = data;
+        for (let i = 0; i < 10; i++) {
+          var joined_2 = this.state.credit_array.concat('$' + credit_data[i].amount + ' ' + credit_data[i].description + ' ' + credit_data[i].date.slice(0, 10));
+          this.setState({
+            credit_array: joined_2,
+            creditBalance: this.state.creditBalance + credit_data[i].amount,
+            accountBalance: this.state.accountBalance + credit_data[i].amount
+          })
+        }
+      });
+    // this.setState({
+    //   accountBalance: this.state.debitBalance
+    // })
+  }
+
+  //////////////////////////////////////////////////////////////////////////////ADD CREDIT IMPLEMENTATION///////////////////////////////////////////////////////////////////////
+  //Event Handler for Description  
+  credit_update = (event) => {
+    this.setState({
+      credit_statement: event.target.value
+    });
+  }
+  //Event handler for Amount
+  credit_numeric_update = (event) => {
+    this.setState({
+      credit_value: event.target.value
+    });
+  }
+  //When Add Statement is pressed, do the following function:
+  addCredit = () => {
+    //Adding Date Portion
+    var today = new Date(); //Accessing Date constructor
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate()); //Grabbing Date YYYY-MM-DD
+    this.setState(prev => {
+      return {
+        credit_statement: '',
+        credit_array: prev.credit_array.concat("$" + prev.credit_value + " " + prev.credit_statement + " " + date)
+      };
+    });
+    //Adding the credit value to the total credit balance
+    let conversion = parseFloat(this.state.credit_value) //Converting string input into a float number
+    this.setState(prev => {
+      return {
+        creditBalance: prev.creditBalance + conversion, //Add the inputted value into the total credit value
+        accountBalance: prev.accountBalance + conversion // Add money to account balance when adding more credit
+      };
+    });// EQUIVALENT OF THIS CODE: this.state.creditBalance += conversion;
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  ////////////////////////////////////////////////////////////////////////////ADD DEBIT IMPLEMENTATION/////////////////////////////////////////////////////////////////////////////////////////
+  //Event Handler for Description  
+  debit_update = (event) => {
+    this.setState({
+      debit_statement: event.target.value
+    });
+  }
+  //Event handler for Amount
+  debit_numeric_update = (event) => {
+    this.setState({
+      debit_value: event.target.value
+    });
+  }
+  //When Add Statement is pressed, do the following function:
+  addDebit = () => {
+    //Adding Date Portion
+    var today = new Date(); //Accessing Date constructor
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate()); //Grabbing Date YYYY-MM-DD
+    this.setState(prev => {
+      return {
+        debit_statement: '',
+        debit_array: prev.debit_array.concat("$" + prev.debit_value + " " + prev.debit_statement + " " + date)
+      };
+    });
+    //Adding the debit value to the total debit balance
+    let conversion = parseFloat(this.state.debit_value) //Converting string input into a float number
+    this.setState(prev => {
+      return {
+        debitBalance: prev.debitBalance + conversion, //Add the inputted value into the total debit value
+        accountBalance: prev.accountBalance - conversion // Remove money from account when adding debit
+      };
+    });// EQUIVALENT OF THIS CODE: this.state.debitBalance += conversion;
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Update state's currentUser (userName) after "Log In" button is clicked
   mockLogIn = (logInInfo) => {
@@ -64,6 +182,13 @@ class App extends Component {
       updateAccountBalance={this.updateAccountBalance}
       updateCredit={this.updateCredit}
       creditBalance={this.state.creditBalance}
+      credit_array={this.state.credit_array}
+      credit_value={this.state.credit_value}
+      credit_statement={this.state.credit_statement}
+      credit_update={this.credit_update}
+      credit_numeric_update={this.credit_numeric_update}
+      debitBalance = {this.state.debitBalance}
+      addCredit={this.addCredit}
     />);
     //Creating Debit Implementation
     const DebitsComponent = () =>
@@ -72,7 +197,15 @@ class App extends Component {
         accountBalance={this.state.accountBalance}
         updateAccountBalance={this.updateAccountBalance}
         updateDebit={this.updateDebit}
-        debitBalance={this.state.debitBalance} />
+        debitBalance={this.state.debitBalance}
+        debit_array={this.state.debit_array}
+        debit_update={this.debit_update}
+        debit_numeric_update={this.debit_numeric_update}
+        addDebit={this.addDebit}
+        debit_value={this.state.debit_value}
+        debit_statement={this.state.debit_statement}
+        creditBalance = {this.state.creditBalance}
+      />
     );
 
     return (
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,83 +1,14 @@
 // src/components/Debits.js
 //import AccountBalance from './AccountBalance';
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Debits extends Component {
   constructor(props) {
     super(props);
     this.state =
     {
-      statement: '',
-      statement_list: [],
-      debit_value: '',
-      total_debit: 0,
     };
-
-    //BINDING THIS TO MAKE FUNCTIONS WORK
-    this.update = this.update.bind(this);
-    this.addDebit = this.addDebit.bind(this);
-    this.numeric_update = this.numeric_update.bind(this);
-  }
-
-  componentDidMount() {
-    let binData = null;
-    // THIS PORTION PARSES THROUGH DEBITS API AND APPENDS IT TO OUR EMPTY STATEMENT LIST ARRAY
-    fetch('https://moj-api.herokuapp.com/debits')
-      .then(result => result.json())
-      .then(data => {
-        binData = data;
-        for (let i = 0; i < 10; i++) {
-          var joined = this.state.statement_list.concat('$' + binData[i].amount + ' ' + binData[i].description + ' ' + binData[i].date.slice(0, 10));
-          this.setState({
-            statement_list: joined
-          })
-          this.setState({
-            total_debit: this.state.total_debit + binData[i].amount
-          })
-          //Updating Parent Debit Balance 
-          this.props.updateDebit(binData[i].amount) 
-          //Updating Parent Account Balance 
-          this.props.updateAccountBalance((this.props.accountBalance - binData[i].amount))
-        }
-      });
-  }
-
-  update(event) {
-    this.setState({
-      statement: event.target.value
-    });
-  }
-
-  numeric_update(event) {
-    this.setState({
-      debit_value: event.target.value
-    });
-  }
-
-  addDebit() {
-    //Adding Date Portion
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate()); //Grabbing Date YYYY-MM-DD
-    this.setState(prev => {
-      return {
-        statement: '',
-        statement_list: prev.statement_list.concat("$" + prev.debit_value + " " + prev.statement + " " + date)
-      };
-    });
-
-    //Adding the debit value to the total debit balance
-    let convert = parseFloat(this.state.debit_value) //Converting string input into a float number
-    this.setState(prev => {
-      return {
-        total_debit: prev.total_debit + convert //Add the inputted value into the total debit value
-      };
-    });// EQUIVALENT OF THIS CODE: this.state.total_debit += convert;
-
-    //Updating Debit value to parent
-    this.props.updateDebit(parseFloat(this.state.debit_value));
-    // Updating accountBalance value to parent
-    this.props.updateAccountBalance(this.props.accountBalance - (parseFloat(this.state.debit_value)))
   }
 
   render() {
@@ -86,21 +17,24 @@ class Debits extends Component {
         <h1>Debits</h1>
         <ul>
           {
-            this.state.statement_list.map((statements) => {
+            this.props.debit_array.map((statements) => {
               return <li>{statements}</li>
             })
           }
         </ul>
         <label>Amount</label>
-        <input type="text" value={this.state.debit_value} onChange={this.numeric_update} />
+        <input type="text" value={this.props.debit_value} onChange={this.props.debit_numeric_update} />
         <label>Description</label>
-        <input type="text" value={this.state.statement} onChange={this.update} />
+        <input type="text" value={this.props.debit_statement} onChange={this.props.debit_update} />
         <button
           type="button"
-          onClick={this.addDebit}>Add Statement</button>
+          onClick={this.props.addDebit}>Add Statement</button>
         <h2>Total Debit Value: {this.props.debitBalance}</h2>
-        <h3>Account Balance: {this.props.accountBalance}</h3>
+        <h3>Total Credit Value: {this.props.creditBalance}</h3>
+        <h4>Account Balance: {this.props.accountBalance}</h4>
         <Link to="/credits">Credits </Link>
+        <br/>
+        <Link to = "/">Home</Link>
       </div>
     )
   }
