d08b3955c0d0cf470257e2b782e87c6264a9e968
Christopher Pineda <Christopher.Pineda59@myhunter.cuny.edu>
2022-04-08 22:41:37 -0400

    Adding Final touches as well as cleaning up useless code

--- src/App.js
+++ src/App.js
@@ -138,41 +138,15 @@ class App extends Component {
     newUser.userName = logInInfo.userName
     this.setState({ currentUser: newUser })
   }
-
-  updateAccountBalance = balance => {
-    this.setState({
-      accountBalance: balance
-    })
-  }
-  //Adding implementation of updating debitBalance
-  updateDebit = debit => {
-    this.setState({
-      debitBalance: this.state.debitBalance + debit
-    })
-  }
-
-  //Adding implementation of updating creditBalance
-  updateCredit = credit => {
-    this.setState(
-      {
-        creditBalance: this.state.creditBalance + credit
-      }
-    )
-  }
-
   // Create Routes and React elements to be rendered using React components
   render() {
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
-    const UserProfileComponent = () => (
-      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
-    );
+    const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>);
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);  // Pass props to "LogIn" component
     //Creating Credits Implementation
     const CreditsComponent = () =>
     (<Credits
       accountBalance={this.state.accountBalance}
-      updateAccountBalance={this.updateAccountBalance}
-      updateCredit={this.updateCredit}
       creditBalance={this.state.creditBalance}
       credit_array={this.state.credit_array}
       credit_value={this.state.credit_value}
@@ -187,8 +161,6 @@ class App extends Component {
     (
       <Debits
         accountBalance={this.state.accountBalance}
-        updateAccountBalance={this.updateAccountBalance}
-        updateDebit={this.updateDebit}
         debitBalance={this.state.debitBalance}
         debit_array={this.state.debit_array}
         debit_update={this.debit_update}
